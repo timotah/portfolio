beabff6ffd5e12832b3392c32eb55b1cc1cedf54
Tim Radtke <tradtke22@gmail.com>
2023-11-28 19:55:06 -0600

    finishing routing, starting on server development

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
 "use strict";
+const http = require("http");
+const Router = require("./router");
 
-console.log("HELLO");
+const routes = [
+    { path: "/", callback: () => console.log("Home page") },
+    { path: "/projects", callback: () => console.log("Projects page") },
+];
+
+const router = new Router(routes);
+
+const portNumber = 8080;
+
+// Create a server instance
+const server = http.createServer((req, res) => {
+    // Write a response to the client
+    res.write("Response from server");
+    res.statusCode = 200;
+
+    // End the response
+    res.end();
+});
+
+// Setup the server to listen on port 8080
+server.listen(portNumber, () => {
+    console.log("Server is listening on port " + portNumber);
+});
